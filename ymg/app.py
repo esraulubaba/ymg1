ded9a78cb18f2def172cb272177b939e91144fd9
esraulubaba <149939589+esraulubaba@users.noreply.github.com>
2024-12-26 23:47:37 +0300

    Add files via upload

---
+++ ymg/app.py
@@ -0,0 +1,14 @@
+
+def topla(sayi1, sayi2):
+    return sayi1 + sayi2
+
+def carp(sayi1, sayi2):
+    return sayi1 * sayi2
+
+def main():
+    sayi1 = 5
+    sayi2 = 3
+    
+    # Toplama ve çarpma işlemlerini yap
+    toplam = topla(sayi1, sayi2)
+    carpim = carp(sayi1, sayi2)
